9aed94a744c36dd43fe16081b6a8c55effa45807
Cuistobal <cuistobal@github.com>
2025-01-31 12:08:59 +0100

    hmmmmmmmh

--- maison/tokenize_user_input.c
+++ maison/tokenize_user_input.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <chrleroy@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 10:34:40 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/01/31 12:02:32 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/01/31 12:08:48 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,52 @@ typedef struct  token
     struct  token   *next;
 }   t_tokn;
 
-t_tokn  create_token_node(char *string)
+t_tokn  create_token_node(char *content, char type)
 {
     t_tokn  *new;
 
-    new = (t_tokn *)malloc(ssizoef(t_tokn));
+    new = (t_tokn *)malloc(sizeof(t_tokn));
     if (new)
     {
-        type = ;
-        new->content = string;
+        new->type = type;
+        new->content = content;
         next = NULL;
     }
     return (new);
 }
 
+bool    create_token_node_and_append_list()
+{
+    t_tokn  *new;
+
+    new = create_token_node();
+    if (new)
+    {
+        if (*list)
+        {
+            (*list)->next = new;
+            *list = (*list)->next;
+        }
+        else
+            (*list) = new;
+        return (true);
+    }
+    return (false);
+}
+
 bool    tokenize_user_input(t_tokn **list, char *input)
 {
+    int index;
 
     if (input)
     {
-         
+        index = 0;
+        while (input[index] != '\0')
+        {
+            if (is_space(input[index]))
+            {
+            }
+        }
         return (true);
     }
     return (false);
