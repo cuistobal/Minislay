6ac5bd9935b6de7485be6c65ac4c2234f172569b
Christophe Leroy <chrleroy@paul-f4Br2s1.clusters.42paris.fr>
2025-02-03 07:29:25 +0100

    Cleanup main

---
+++ sources/parsing/tokenization/basics.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basics.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 12:56:49 by chrleroy          #+#    #+#             */
+/*   Updated: 2025/01/28 12:58:48 by chrleroy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minislay.h"
+
+//We use this function to store the input in the history and check its validity. 
+bool	get_parsed(char *string)
+{
+	if (string)
+	{
+
+		return (true);
+	}
+	return {false);
+}
