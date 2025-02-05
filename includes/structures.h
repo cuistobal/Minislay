ad97717fd6d8b12a68966ced3c92482695df0149
Christophe Leroy <chrleroy@paul-f5Br1s1.clusters.42paris.fr>
2025-02-05 09:12:12 +0100

    Working on tokenization mat1 0502 Paul

--- includes/structures.h
+++ includes/structures.h
@@ -6,34 +6,12 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:51:57 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/02/04 14:07:20 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/02/04 14:13:18 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
-
-//We use this enum to append the right token type
-enum token_type
-{
-    TOKEN_WORD,         // General word or argument
-    TOKEN_ASSIGNMENT,   // VAR=value
-    TOKEN_REDIR_IN,     // <
-    TOKEN_REDIR_OUT,    // >
-    TOKEN_APPEND,       // >>
-    TOKEN_HEREDOC,      // <<
-    TOKEN_PIPE,         // |
-    TOKEN_SINGLE_QUOTE, // '
-    TOKEN_DOUBLE_QUOTE, // "
-    TOKEN_DOLLAR,       // $ or $?
-    TOKEN_NEWLINE,      // \n
-    TOKEN_EOF,          // End of input
-    TOKEN_AND_IF,       // &&
-    TOKEN_OR_IF,        // ||
-    TOKEN_PAREN_OPEN,   // (
-    TOKEN_PAREN_CLOSE,  // )
-    TOKEN_WILDCARD      // *
-};
 
 typedef enum s_type 
 {
