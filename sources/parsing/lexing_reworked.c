fb5149430b91d428813b659ed3c114888748765b
chrleroy <you@example.com>
2025-02-23 13:30:42 +0100

    Algo ok (?) -> Branch AST

--- sources/parsing/lexing_reworked.c
+++ sources/parsing/lexing_reworked.c
@@ -1,5 +1,6 @@
 #include "minislay.h"
 
+/*
 //Assesses if we're dealing with the last element of the list.
 static bool	has_next_elem(t_tokn *current)
 {
@@ -12,7 +13,7 @@ static bool	has_next_elem(t_tokn *current)
 static bool	valid_lexeme(t_tokn *current, int min, int max)
 {
 	return (current && (current->type >= min && current->type <= max));	
-}
+}*/
 
 bool	consume_token(t_tokn **current)
 {
@@ -80,9 +81,9 @@ bool	build_ast(t_tree **ast, t_tokn *tokens, t_tokn *current)
 }*/
 
 //
-static void	print_tokens(t_tokn *start, t_tokn *end)
+static void	print_tokens(t_tokn *start, t_tokn *end, int calls, char *msg)
 {
-	printf("PRINT TOKENS	->	");
+	printf("\nPRINT TOKENS in %s	@	%d	->	\n", msg, calls);
 	while (start && start != end)
 	{
 		printf("%s ", start->value);
@@ -103,10 +104,20 @@ bool	parse_script(t_tokn *head)
 	{
 		if (!parse_command_list(&current))
 			printf("%s	&&	%d\n", current->value, current->type);
-		return (parse_command_list(&current));
+		//return (parse_command_list(&current));
 	}
 	return (current != head);
 }
+
+static int	calls = 0;
+
+//
+//	Il faut traverser l arbre en meme temps que la string && creer des branches
+//	vers la gauche tant que l'on a pass rencontre le premier operateur le plus
+//	significatif (PIPE | LAND | LORR)
+//	Donc, tant qu'on est dans un sous-shell, on allour recursivement des noeuds
+//	vers la gauche.
+//
 
 //bool	parse_command_list(t_tree **ast, t_tokn **current)
 bool	parse_command_list(t_tokn **current)
@@ -115,30 +126,68 @@ bool	parse_command_list(t_tokn **current)
 //	t_tokn	*operator;
 
 	save = *current;
-	if (parse_command(current))
+	
+	//print_tokens(save, NULL, calls, "ROOT");
+
+	if (*current)
 	{
-        printf("%s  %d\n", (*current)->value, (*current)->type);
-		print_tokens(save, *current);
-//		operator = *current;
-		if ((*current) && ((*current)->type & LORR || (*current)->type & LAND))
+		if (parse_command(current))
 		{
-		//	if (create_ast_node(ast ,*current, save));
-		//	{
-				if (consume_token(current))
-					//return (parse_command_list(&(*ast)->right, current));
-					return (parse_command_list(current));
-				/*Impossible to get there since consume_token() can dereference
-				 * current->next.
-				 * We could get a LORR | LAND without a following command
-				 * OR
-				 * We could get a LORR | LAND with a following CPAR
-				 *
-				 * Both situations terrifies me.
-				 * Joke apart, we need to secure those scenarios
-				 */
-		//	}
+		
+			//print_tokens(save, *current, calls, "BRANCH");
+		
+			calls++;
+
+			//Creer un noeud left
+			printf("\nCREATE LEFT NODE");
+			print_tokens(save, *current, calls, "BRANCH");
+
+			if ((*current) && ((*current)->type & CPAR))
+				return (true);
+				//while ((*current) && ((*current)->type & CPAR))
+			//	consume_token(current);
+			if ((*current) && ((*current)->type & LORR || (*current)->type & LAND))
+			{
+				printf("\nCREATE ROOT NODE");
+				printf("\n%s\n", (*current)->value);	
+				//Retour sur la root
+				//Loger l operateur logique
+
+				//printf("BRANCHING ON				->	%s\n", (*current)->value);
+				//print_tokens(save, NULL, calls);
+			//	if (create_ast_node(ast ,*current, save));
+			//	{
+				//	if (consume_token(current))
+				//	{
+
+				consume_token(current);
+						//Creer un noeud droite
+						//Loger le reste de l'expression
+				
+				printf("\nCREATE RIGHT NODE");
+			//	print_tokens(save, *current, calls, "LEAF && OP");
+				print_tokens(*current, NULL, calls, "");
+						//return (parse_command_list(&(*ast)->right, current));
+						//1	return (parse_command_list(current));
+					/*Impossible to get there since consume_token() can dereference
+					 * current->next.
+					 * We could get a LORR | LAND without a following command
+					 * OR
+					 * We could get a LORR | LAND with a following CPAR
+					 *
+					 * Both situations terrifies me.
+					 * Joke apart, we need to secure those scenarios
+					 */
+			//	}
+			}
+		//	if ((*current)->type & CPAR)
+		//		return (true);
+			//else if ((*current)->type & CPAR)
+			//	return (consume_token(current));
+			return (parse_command_list(current));
+			//return (true);
 		}
-		return (*current);
+	//	return (*current);
 	/* It possibly makes more sense building the left side node after the right
 	 * side node. Hence, we'de remove the upper call to the create_ast_node()
 	 * function.
@@ -147,18 +196,27 @@ bool	parse_command_list(t_tokn **current)
 	//	OR
 	//	return (create_ast_node(&(*ast)->left, *current, save));
 	}
+	//	Edge case	->	current is NULL && different from save -> means that
+	//	parse_command() failed.
 	return (!*current);
 }
 
 bool	parse_command(t_tokn **current)
 {
-//	t_tokn *save;
+	t_tokn *save;
 
-//	save = *current;
-	if ((*current)->type == OPAR)
-		return (parse_compound_command(current));
-	else if (!parse_simple_command(current))
-		return (parse_pipeline(current));
+	save = *current;
+	if (*current)
+	{
+		if ((*current)->type == OPAR)
+		{
+			if (parse_compound_command(current))
+				return (true);
+			*current = save;
+		}
+		if (!parse_simple_command(current))
+			return (parse_pipeline(current));
+	}
 	return (true);
 }
 
@@ -169,32 +227,10 @@ bool	parse_compound_command(t_tokn **current)
 	if ((*current)->type == OPAR)
 	{
 		consume_token(current);
-		if (parse_command(current))
+		if (parse_command_list(current))
 		{
-			if ((*current)->type & OPAR)
-			{
-				if (valid_lexeme(*current, CPAR, LORR | OPAR))
-				{
-                printf("%s  %d\n", (*current)->value, (*current)->type);
-					consume_token(current);
-					return (parse_command(current));
-				}
-			return (false);
-            //	return (consume_token(current));
-			}
-            else if ((*current)->type == CPAR)
-            {
-                consume_token(current);
-                if (*current)
-                {
-                    if (valid_lexeme(*current, WORD, SQTE))
-                        return (parse_command(current));
-                    else if ((*current)->type & PIPE)
-                        return (parse_pipeline(current));
-                }
-                return (!*current);
-            }
-		//	return (valid_lexeme(*current, OPAR, LORR | OPAR));
+			if ((*current)->type & CPAR)
+				return (consume_token(current));
 		}
 	}
 	return (false);
@@ -223,21 +259,25 @@ bool	parse_redirection(t_tokn **current)
 	t_tokn	*save;
 
 	save = NULL;
-    if ((*current)->type >= IRED && (*current)->type <= (ARED | OPAR))
+	if (*current)
 	{
-		save = *current;
-		consume_token(current);
-        if ((*current))
+    	if ((*current)->type >= IRED && (*current)->type <= (ARED | OPAR))
 		{
-			if ((*current)->type & WORD)
-				return (consume_token(current));
-			printf("syntax error: Unexpected %s token after %s token.\n", (*current)->value, save->value);
-			return (false);
-		}
-		*current = save;
-		printf("syntax error: Unexpected EOF token after %s token.\n", save->value);
-    }
-    return (false);
+			save = *current;
+			consume_token(current);
+    	    if ((*current))
+			{
+				if ((*current)->type & WORD)
+					return (consume_token(current));
+				printf("syntax error: Unexpected %s token after %s token.\n", (*current)->value, save->value);
+				return (false);
+			}
+			*current = save;
+			printf("syntax error: Unexpected EOF token after %s token.\n", save->value);
+    	}
+	}
+    //return (false);
+    return (!*current);
 }
 
 // Edge case $ seul + gerer 
@@ -302,6 +342,7 @@ bool	parse_pipeline(t_tokn **current)
 			printf("Invalid syntax, expected token after PIPE token.\n");	
 			return (false);
 		}
+		/*
 		else if ((*current)->type & OPAR || (*current)->type & CPAR)
 		{
 			if (valid_lexeme(*current, LAND, LORR | OPAR))
@@ -310,7 +351,7 @@ bool	parse_pipeline(t_tokn **current)
                 if (has_next_elem(*current))
 				    return (parse_command(current));
             } 
-            /*
+            *//*
 			if (has_next_elem(*current) && valid_lexeme(*current, LAND, LORR | OPAR))
 			{
 				consume_token(current);
@@ -325,8 +366,8 @@ bool	parse_pipeline(t_tokn **current)
 			}
             printf("%s\n", (*current)->value);*/
             return (true); //A retravailler
-		}
-		return (valid_lexeme(*current, WORD, SQTE | OPAR));
+//		}
+//		return (valid_lexeme(*current, WORD, SQTE | OPAR));
 	}
 	return (!*current);
 }
