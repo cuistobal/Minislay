9c99f33a20ea5fd7a948072a64fb9fa14207caaa
chrleroy <you@example.com>
2025-03-06 11:53:38 +0100

    switch

--- sources/builtins/echo/echo.c
+++ sources/builtins/echo/echo.c
@@ -1,5 +1,7 @@
 #include "minislay.h"
 
+//Echo built-in. Prints the argument followed by a new_line unless the flag is
+//spefied.
 bool	echo(const char **arguments, bool flag)
 {
 	while (*arguments)
