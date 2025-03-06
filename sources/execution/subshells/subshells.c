9ad4ac0345c81029ef198e9b47790e5b458544ea
chrleroy <you@example.com>
2025-03-06 11:11:15 +0100

    post merge subshell->main

--- sources/execution/subshells/subshells.c
+++ sources/execution/subshells/subshells.c
@@ -56,7 +56,7 @@ bool	handle_subshell(t_shel *minishell, t_tree *ast)
 	subshell_command = NULL;
 	if (prompt(&subshell_command, ast))
 	{
-		//printf("SUBSHELL COMMAND	->	%s\n", subshell_command);
+		printf("SUBSHELL COMMAND	->	%s\n", subshell_command);
 		if (get_minishelled(&copy, subshell_command))
 		{
 			free(subshell_command);
