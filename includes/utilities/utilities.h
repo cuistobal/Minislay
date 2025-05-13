30b50dd23e8f523857df1502bd09387efeeab085
chrleroy <you@example.com>
2025-05-13 09:48:31 +0200

    few fixes - mat4 1305 42UP

--- includes/utilities/utilities.h
+++ includes/utilities/utilities.h
@@ -6,7 +6,7 @@
 /*   By: chrleroy <chrleroy@student42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 08:23:36 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/04/26 09:12:25 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/05/13 09:44:19 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,5 +80,8 @@ bool	error_message(char *message);
 
 //tests
 void	free_tree(t_tree *ast);
+
+//update_key_value.c
+void	update_key_value(t_shel *minishel, char *key, char *new_value);
 
 #endif
--- sources/builtins/cd/cd.c
+++ sources/builtins/cd/cd.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:26:44 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/05/13 09:27:05 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/05/13 09:47:30 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ void	cd(t_shel *minishell, char *path)
 	char	*value;
 	char	*current_directory;
 
+	memset(current_directory, 0, BUFFER_SIZE);
+
 	if (!minishell || !path)
 		return ;
+
 	current_directory = getcwd(current_directory, BUFFER_SIZE);
 
 	//Using cd on the current working directory triggers a specific behaviour
@@ -28,16 +31,18 @@ void	cd(t_shel *minishell, char *path)
 	if (strcmp(current_directory, path) == 0)
 		printf("%s\n", current_directory);
 
+	//
+
 	else 
 	{
 		if (chdir(path) != 0)
-			error_message("bash: cd: dqwqwdqwdqdw: No such file or directory");
+			printf("bash: cd: %s: No such file or directory\n", path);
 		else
 		{
-			find_key(minishell, &value, PWD);
-			
-			update_key(minishell, PWD, );
-			update_key(minishell, PWD, );
+			update_key_value(minishell, "OLDPWD", current_directory);
+			memset(current_directory, 0, BUFFER_SIZE);
+			current_directory = getcwd(current_directory, BUFFER_SIZE);
+			update_key_value(minishell, "PWD", current_directory);
 		}
 	}
 	return ;
