84b576513f39b79935e745b43e1138569ac699cb
Cuistobal <cuistobal@github.com>
2025-05-17 10:02:53 +0200

    Ca repart - mat1 1605 Maison

--- sources/execution/processes/ipc_module.c
+++ sources/execution/processes/ipc_module.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:31:39 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/05/16 18:14:24 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/05/17 09:57:02 by cuistobal        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,6 @@ int	handle_communication_in_parent(t_exec **node)
 //
 int	handle_redirections(t_exec **execution, int original_stds[2])
 {
-    original_stds[0] = dup(STDIN_FILENO);
-    if (original_stds[0] == -1)
-		return (GENERAL_ERROR);
-    original_stds[1] = dup(STDOUT_FILENO);
-    if (original_stds[1] == -1)
-		return (GENERAL_ERROR);
 	if ((*execution)->redirections[INFILE] != STDIN_FILENO)
 	{
 		if (dup2((*execution)->redirections[INFILE], STDIN_FILENO) != 0)
