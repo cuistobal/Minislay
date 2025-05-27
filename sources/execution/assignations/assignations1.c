b22fa74d3bb251bf31346c2f51a0849b43087f3a
chrleroy <you@example.com>
2025-05-27 12:59:23 +0200

    Pipe rework - 2705 apm1 42UP

--- sources/execution/assignations/assignations1.c
+++ sources/execution/assignations/assignations1.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 08:40:10 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/05/27 12:20:11 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/05/27 12:59:10 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,37 +26,3 @@ void	add_key_to_local(t_shell **minishell, t_tokn *assignations)
 		move_pointer(&assignations);
 	}
 }
-
-/*
-// This function sends the assignation token to the local env list.
-void	handle_assignations(t_shell **minishell, t_tokn **source)
-{
-	t_tokn	*copy;
-	t_tokn	*expand_tail;
-	t_tokn	*expand_head;
-	t_tokn	*assignation_tail;
-	t_tokn	*assignation_head;
-
-	if (!source || !*source)
-		return ;
-	copy = *source;
-	expand_tail = NULL;
-	expand_head = NULL;
-	assignation_head = NULL;
-	assignation_tail = NULL;
-	while (copy)
-	{
-		if (is_state_active(copy->type, EQUL))
-			append_token_list(&assignation_head, &assignation_tail, copy);	
-		else	
-			append_token_list(&expand_head, &expand_tail, copy);
-		copy = copy->next;
-	}
-	if (expand_head)
-		*source = expand_head;
-	if (expand_tail)
-		expand_tail->next = NULL;	
-	if (assignation_tail)
-		assignation_tail->next = NULL;
-}
-*/
