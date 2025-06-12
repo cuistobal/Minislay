8e340ef6afd4f433314c82e286fc442a655998bb
chrleroy <you@example.com>
2025-06-12 20:19:38 +0200

    Normed back - ready to ship

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: ynyamets <ynyamets@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:21:53 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/06/12 20:12:15 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/06/12 20:15:55 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,39 +41,14 @@ int	my_exit(char **args)
 	if (!*args)
 		return (SUCCESS);
 	if (!is_numeric(args[0]))
-		return (write(2, "minislay: exit: numeric argument required\n", 43), \
-				BUILTINS);
+	{
+		write(2, "minislay: exit: numeric argument required\n", 43);
+		return (BUILTINS);
+	}
 	else if (args[1])
-		return (write(2, "minislay: exit: too many arguments\n", 35), );
+		return (write(2, "minislay: exit: too many arguments\n", 35), 1);
 	else
 		code = atol(args[0]);
 	code = (code % 256 + 256) % 256;
 	return (EXIT_CODE);
 }
-
-int	my_exit(char **args)
-{
-	long	code;
-
-	write(1, "exit\n", 5);
-	if (!*args)
-	{
-		append_exit_code(minishell, SUCCESS, true);
-		return (EXIT_CODE);
-	}
-	if (!is_numeric(args[0]))
-	{
-		append_exit_code(minishell, BUILTINS, true);
-		return (write(2, EXIT_ARG, strlen(EXIT_ARGS)), EXIT_CODE);
-	}
-	else if (args[1])
-	{
-		append_exit_code(minishell, GENERAL_ERROR, true);
-		return (write(2, EXIT_ARGS, strlen(EXIT_ARGS)),	EXIT_CODE);
-	}
-	code = atol(args[0]);
-	code = (code % 256 + 256) % 256;
-	append_exit_code(minishell, code, true);
-	return (EXIT_CODE);
-}
-
