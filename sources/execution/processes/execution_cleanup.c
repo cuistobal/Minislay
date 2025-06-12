11b5a9a1df15c52e15c6af5002fc43ba545c2c81
chrleroy <you@example.com>
2025-06-12 12:31:50 +0200

    Normed back - ready to ship

--- sources/execution/processes/execution_cleanup.c
+++ sources/execution/processes/execution_cleanup.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 15:37:06 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/06/12 12:07:35 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/06/12 12:29:07 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	child_cleanup(t_shell **minishell, t_exec *node, int cmd)
 	close_command_redirs(node);
 	free_execution_node((*minishell)->execution);
 	free_tree((*minishell)->ast);
-	while (index < cmd + 1)
+	while (index < cmd)
 	{
 		close((*minishell)->pipefd[index][READ_END]);
 		close((*minishell)->pipefd[index][WRITE_END]);
