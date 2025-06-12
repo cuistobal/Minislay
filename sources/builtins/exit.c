cfaaff68312b0a9a0cc90258feeffc3471807108
chrleroy <you@example.com>
2025-06-12 20:09:29 +0200

    Normed back - ready to ship

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: ynyamets <ynyamets@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:21:53 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/06/12 20:07:43 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/06/12 20:09:18 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,3 +52,30 @@ int	my_exit(char **args)
 	code = (code % 256 + 256) % 256;
 	return (EXIT_CODE);
 }
+
+int	my_exit(t_shell *minishell, char **args)
+{
+	long	code;
+
+	write(1, "exit\n", 5);
+	if (!*args)
+	{
+		append_exit_code(minishell, SUCCESS, true);
+		return (EXIT_CODE);
+	}
+	if (!is_numeric(args[0]))
+	{
+		append_exit_code(minishell, BUILTINS, true);
+		return (write(2, EXIT_ARG, strlen(EXIT_ARGS)), EXIT_CODE);
+	}
+	else if (args[1])
+	{
+		append_exit_code(minishell, GENERAL_ERROR, true);
+		return (write(2, EXIT_ARGS, strlen(EXIT_ARGS)),	EXIT_CODE);
+	}
+	code = atol(args[0]);
+	code = (code % 256 + 256) % 256;
+	append_exit_code(minishell, code, true);
+	return (EXIT_CODE);
+}
+
